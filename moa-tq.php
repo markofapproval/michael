a9035073178a669a9302732bd49b50154a64c27e
Mark Hunter <mark@markofapproval.com>
2021-06-18 13:04:28 +1000

    Michael Project

--- moa-tq.php
+++ moa-tq.php
@@ -1,9 +1,14 @@
 <?php
 /*
- * Plugin Name: Tsquared Tweaks
+ * Plugin Name: MOA Tsquared Tweaks
  */
 
 class MOA_TQ{
+
+	public function init(){
+		add_action('pre_get_posts', array($this, 'modify_shop_query'));
+
+	}
 
 	public function enqueue_scripts(){
 
@@ -13,4 +18,26 @@ class MOA_TQ{
 
 	}
 
+	/**
+	 * @param $query WP_Query
+	 */
+	public function modify_shop_query( $query ){
+		//also works for Product Category or Brand
+
+		if(is_post_type_archive('product'))
+		{
+			if(!is_admin() && is_main_query()){
+				
+			}
+
+
+		}
+
+	}
+
+
+
 }
+
+$moa_tq = new MOA_TQ();
+$moa_tq->init();
